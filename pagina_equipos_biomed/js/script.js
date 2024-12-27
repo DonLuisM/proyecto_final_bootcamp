763b675bd700776ef133a3454955360c64ae18b1
DonLuisM <lfmb03@outlook.com>
2024-12-27 01:11:40 -0500

    updates

--- pagina_equipos_biomed/js/script.js
+++ pagina_equipos_biomed/js/script.js
@@ -1,7 +1,16 @@
 // Función para validar contenido en el input
+function buscarVacio(){
+    var buscar = document.getElementById('buscar').value;
+    var resultadosDiv = document.getElementById('resultados_busq');
 
-
-
+    if(buscar === ""){
+        resultadosDiv.innerHTML = "<p style='color: red'>Por favor, ingrese el nombre de un equipo</p>";
+    } else {
+        resultadosDiv.innerHTML = `<p>Resultados encontrados para: <b>${buscar}</b></p>`;
+        localStorage.setItem('busqueda', buscar);
+        return true;
+    }
+}
 // Función para realizar la solicitud y mostrar en la búsqueda
 
 
