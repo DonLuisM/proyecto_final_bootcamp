328faea86893911a21f2f41dedcf5272120a13f1
DonLuisM <lfmb03@outlook.com>
2024-12-29 00:50:23 -0500

    updates_js

--- pagina_equipos_biomed/js/script.js
+++ pagina_equipos_biomed/js/script.js
@@ -1,4 +1,4 @@
-// Función para validar contenido en el input
+// Función para validar contenido en el input de búsqueda
 function buscarVacio(){
     var buscar = document.getElementById('buscar').value;
     var resultadosDiv = document.getElementById('resultados_busq');
@@ -11,6 +11,16 @@ function buscarVacio(){
         return true;
     }
 }
+// ? Me falta revisar bien la parte de almacenamiento de resultados, para pasar de inicio a búsqueda con el input del usuario y las validaciones previas en inicio
+
+// Función para la clasificación seleccionada y mostrar botones ocultos
+function elegirClasif(clasificacion) {
+    document.getElementById('clasificacion_eq').innerText = clasificacion;
+
+    // Acciones de botones ocultos al seleccionar la clasificación
+}
+
+
 // Función para realizar la solicitud y mostrar en la búsqueda
 
 
